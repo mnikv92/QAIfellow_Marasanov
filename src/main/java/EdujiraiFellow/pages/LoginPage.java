d7509075a3204bbb2da305a329723262a5f9b9f2
Marasanov Nikita <nikmarasanov@gmail.com>
2024-12-04 18:30:54 +0700

    Починил Логин, почистил мусор

--- src/main/java/EdujiraiFellow/pages/LoginPage.java
+++ src/main/java/EdujiraiFellow/pages/LoginPage.java
@@ -12,10 +12,18 @@ public class LoginPage {
     private final SelenideElement loginInput = $x("//input[@id='login-form-username']");
     private final SelenideElement passwordInput = $x("//input[@id='login-form-password']");
     private final SelenideElement loginButton = $x("//input[@value='Войти']");
+    private final SelenideElement loginError = $x("//div[@id='usernameerror']");
 
     public void loginIntoEdujira(String login, String password) {
         loginInput.shouldBe(Condition.visible, Duration.ofSeconds(5)).sendKeys(login);
         passwordInput.shouldBe(Condition.visible, Duration.ofSeconds(5)).sendKeys(password);
         loginButton.click();
+
+        if (loginError.exists()) {
+            sleep(2000);
+            loginInput.shouldBe(Condition.visible, Duration.ofSeconds(5)).sendKeys(login);
+            passwordInput.shouldBe(Condition.visible, Duration.ofSeconds(5)).sendKeys(password);
+            loginButton.click();
+        }
     }
 }
--- src/test/java/EdujiraiFellow/CucumberRunnerTest.java
+++ src/test/java/EdujiraiFellow/CucumberRunnerTest.java
@@ -6,16 +6,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
-
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("src/main/java/EdujiraiFellow/steps")
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/HW4.feature")
-//@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "EdujiraiFellow/hooks")
-//@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@login")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "src/test/resources/HW4.feature")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "EdujiraiFellow")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
-//@ConfigurationParameter(key = Constants.FILTER_NAME_PROPERTY_NAME, value = ".*тест")
 
 public class CucumberRunnerTest {
 }
