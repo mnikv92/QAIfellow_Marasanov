80aa00f882ed85bca561eb7f51a5c2520cbd1cb4
Marasanov Nikita <nikmarasanov@gmail.com>
2024-12-04 18:36:10 +0700

    Починил Логин, почистил мусор

--- src/test/java/EdujiraiFellow/CucumberRunnerTest.java
+++ src/test/java/EdujiraiFellow/CucumberRunnerTest.java
@@ -1,6 +1,6 @@
 package EdujiraiFellow;
 
-import io.cucumber.junit.platform.engine.Constants;;
+import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
