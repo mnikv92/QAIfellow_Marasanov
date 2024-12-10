37b6977fea7b785777b58b9504f14354b8110cab
Marasanov Nikita <nikmarasanov@gmail.com>
2024-12-10 16:49:21 +0700

    Tasks 1-4 done

--- src/main/java/ru/iFellow/api/Specifications.java
+++ src/main/java/ru/iFellow/api/Specifications.java
@@ -9,20 +9,11 @@ import io.restassured.specification.ResponseSpecification;
 
 public class Specifications {
 
-    // создает и возвр объект RequestSpecification, кот задает общие настройки http-запросов
     public static RequestSpecification baseRequestSpec(String url) {
         return new RequestSpecBuilder()
                 .setBaseUri(url)
                 .setContentType(ContentType.JSON)
                 .log(LogDetail.BODY)
-                .build();
-    }
-
-    // созд и возвр объект ResponseSpecification, кот задает ожидания  и настройки для ответов от сервера
-    public static ResponseSpecification baseResponseSpecSuccess() {
-        return new ResponseSpecBuilder()
-                .expectStatusCode(200)
-                .log(LogDetail.ALL)
                 .build();
     }
 }
